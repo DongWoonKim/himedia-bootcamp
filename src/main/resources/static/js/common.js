e30b5f6b9665037c68921aa81ba31b2a6c011a09
DongWoonKim <01030985843z@naver.com>
2025-05-01 12:44:42 +0900

    update

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     const $topBtn = $('.scroll-to-top');
 
     $('#content').on('scroll', function () {
-        console.log($('#content').scrollTop())
         if ($('#content').scrollTop() > 300) {
             $topBtn.fadeIn();
         } else {
@@ -12,5 +11,5 @@ $(document).ready(function () {
 });
 
 function scrollToTop() {
-    $('html, body').animate({ scrollTop: 0 }, 500); // 500ms 동안 부드럽게 스크롤
+    $('#content').animate({ scrollTop: 0 }, 500); // 500ms 동안 부드럽게 스크롤
 }
