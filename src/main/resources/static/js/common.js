3909f68268a0ee0049d412ffc8832fc41f70ba07
DongWoonKim <01030985843z@naver.com>
2025-05-02 16:31:56 +0900

    update

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -8,8 +8,19 @@ $(document).ready(function () {
             $topBtn.fadeOut();
         }
     });
+
 });
 
 function scrollToTop() {
     $('#content').animate({ scrollTop: 0 }, 500); // 500ms 동안 부드럽게 스크롤
+}
+
+function copyAddress() {
+    const text = document.getElementById("address-text").textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert("주소가 복사되었습니다!");
+    }).catch(err => {
+        alert("복사에 실패했습니다.");
+        console.error(err);
+    });
 }
