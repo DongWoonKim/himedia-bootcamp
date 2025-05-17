cb0a91efd03308ace12a94d56bac91806e45b449
DongWoonKim <01030985843z@naver.com>
2025-05-17 16:13:04 +0900

    image slides

---
+++ src/main/resources/static/js/slide.js
@@ -0,0 +1,47 @@
+$(document).ready(() => {
+    setInterval(function() {
+        slide(1);
+    }, 1500);
+
+    $('.slider-1 .side-btns > div:first-child').click( function () {
+        slide(-1);
+    });
+
+    $('.slider-1 .side-btns > div:last-child').click( function () {
+        slide(1);
+    });
+});
+
+function slide(move){
+    // 현재 div이면서 active클래스를 가진 요소를 가져와라..
+    let $cur = $('.slider-1 .slides > div.active');
+    let $next = null;
+    // 현재 페이지 점
+    let $curDot = $('.pages > div.active');
+    let $nextDot = null;
+
+    if(move === -1) { // 이전 div요소
+        $next = $cur.prev();
+        $nextDot = $curDot.prev();
+
+        if($next.length === 0) {
+            $next = $('.slider-1 .slides > div:last-child');
+            $nextDot = $('.pages > div:last-child');
+        }
+
+    } else if(move === 1) { // 다음 div요소
+        // 다음 요소 접근
+        $next = $cur.next();
+        $nextDot = $curDot.next();
+        // 마지막 요소에서 다음으로 접근하려 할 때
+        if($next.length === 0) {
+            $next = $('.slider-1 .slides > div:first-child');
+            $nextDot = $('.pages > div:first-child');
+        }
+    }
+
+    $cur.removeClass('active');
+    $next.addClass('active');
+    $curDot.removeClass('active');
+    $nextDot.addClass('active');
+}
